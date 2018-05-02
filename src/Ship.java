59dfcc9e0b855905a8fc5961af81143a69f81619
3jackattack3 <3jackattack3@gmail.com>
2018-05-02 14:26:05 -0400

    working on ship class

--- src/Ship.java
+++ src/Ship.java
@@ -5,7 +5,36 @@
 */
 
 public class Ship {
-    public static void main(String args[]) {
+        private int length;
+        private String name;
+        private int[] location;
+        private int[] lives;
+        private int totalLives;
+        private int[] direction;
 
-    }
+        public Ship(int length, String name, String coordinates, int[] direction){
+            this.length = length;
+            this.name = name;
+            this.location = new int[2];
+
+            int x = (int) (coordinates.charAt(0) - 'A');
+            int y = (int) (coordinates.charAt(1));
+            this.location[0] = x;
+            this.location[1] = y;
+            this.lives = new int[length];
+
+            for(int i = 0; i < this.length; i++){
+                this.lives[i] = 1;
+            }
+
+            this.totalLives = length;
+
+            for(int j = 0; j < 2; j++){ {
+                this.direction[j] = direction[j];
+            }
+        }
+
+        public static int checkHit(int x, int y){
+
+        }
 }
