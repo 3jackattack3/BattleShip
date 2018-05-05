8da19c8e0a3897651e84f6d45f3f9c70f68917c3
3jackattack3 <3jackattack3@gmail.com>
2018-05-05 10:45:31 -0400

    added psuedocode comments in main

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,27 @@ public class Main {
         Board myBoard = new Board("ships");
 
         System.out.println(myBoard.getShipName(0));
+
+        int choice = 0;
+
+        //gui prompts user for ship locations and directions
+        //modifies the file accordingly
+        //cpu randomly places ships
+
+        //while(choice > -1){
+            //asks user for target location
+            //computerBoard.checkHit
+            //tells the user if it hit or not
+            //displays in gui
+            //checks if any ships have been destroyed and updates score
+            //checks if anyone has won
+
+            //prompts cpu for target location
+            //userBoard.checkHit
+            //tells the user if it was a hit or a miss
+            //display in gui?
+            //checks if any ships have been destroyed and updates scores
+            //checks if anyone has won
+        //}
     }
 }
