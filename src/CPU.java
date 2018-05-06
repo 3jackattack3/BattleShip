51bc61245930b2d8aa3bc49376cedf26d44ba880
3jackattack3 <3jackattack3@gmail.com>
2018-05-06 12:12:51 -0400

    working on cpu!

---
+++ src/CPU.java
@@ -0,0 +1,43 @@
+/*
+    Jack Defay
+    CPU.java
+    5/6/2018
+*/
+
+public class CPU {
+
+    private Board computerBoard;
+
+    private int[] listOfShipLengths;
+
+    //private int difficulty;
+
+    public CPU(Board computerBoard, int[] listOfShipLengths){
+        this.computerBoard = computerBoard;
+        this.listOfShipLengths = listOfShipLengths;
+    }
+
+    public String pickTarget(Board userBoard, int numDestroyedShips){
+        int numShipsOnBoard = numShipsOnBoard(userBoard);
+
+        if(numDestroyedShips == numShipsOnBoard){
+            return randomSelection();
+        }
+    }
+
+    public int numShipsOnBoard(Board userBoard){
+        int numShipsOnBoard = 0;
+
+        //searches board for number of ships
+
+        return numShipsOnBoard;
+    }
+
+    private String randomSelection(){
+        return convertCoordinates(new int[]{0,0}); //return a random coordinate in the form {capital letter,number}
+    }
+
+    private String convertCoordinates(int[] intCoordinates){
+        return " "; //reads in two integer coordinates and converts to the form {capital letter, number}
+    }
+}
